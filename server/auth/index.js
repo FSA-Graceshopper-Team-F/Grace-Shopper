c5ef5add1b20c0e56e219b1e419b23284475490c
Lionel Verrier <lgv3lee@gmail.com>
2023-01-11 11:43:31 -0500

    quick change, auth

--- server/auth/index.js
+++ server/auth/index.js
@@ -17,7 +17,7 @@ router.post('/signup', async (req, res, next) => {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists');
     } else {
-      next(err);
+      next(err)
     }
   }
 });
