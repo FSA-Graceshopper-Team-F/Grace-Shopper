7900aad636cdf4299809bc25a3f1a89c7b7eced5
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-10 22:11:07 -0500

    single component view working

--- client/features/product/singleProductSlice.js
+++ client/features/product/singleProductSlice.js
@@ -1,9 +1,9 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchProductAsync = createAsyncThunk("getProduct", async () => {
+export const fetchProductAsync = createAsyncThunk("getProduct", async (id) => {
 	try {
-		const { data } = await axios.get("/api/products/:productId");
+		const { data } = await axios.get(`/api/products/${id}`);
 		return data;
 	} catch (error) {
 		console.error(error);
@@ -21,7 +21,7 @@ const singleProductSlice = createSlice({
 	},
 });
 
-export const selectProducts = (state) => {
-	return state.products;
+export const selectSingleProduct = (state) => {
+	return state.singleProduct;
 };
 export default singleProductSlice.reducer;
