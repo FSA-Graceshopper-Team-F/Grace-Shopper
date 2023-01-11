579d13feabe28d2e2f3ae11601683b841f272932
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-11 15:26:16 -0500

    secured users route for admin added missing mod file

--- server/api/users.js
+++ server/api/users.js
@@ -5,15 +5,17 @@ module.exports = router;
 router.get("/", async (req, res, next) => {
 	const user = await User.findByToken(req.headers.authorization);
 	try {
-    if(user.isAdmin){
-      const users = await User.findAll({
-        // explicitly select only the id and email fields - even though
-        // users' passwords are encrypted, it won't help if we just
-        // send everything to anyone who asks!
-        attributes: ["id", "email"],
-      });
-      res.json(users);
-    }
+		if (user.isAdmin) {
+			const users = await User.findAll({
+				// explicitly select only the id and email fields - even though
+				// users' passwords are encrypted, it won't help if we just
+				// send everything to anyone who asks!
+				attributes: ["id", "email"],
+			});
+			res.json(users);
+		} else {
+			res.json([{ email: "access denied" }]);
+		}
 	} catch (err) {
 		next(err);
 	}
