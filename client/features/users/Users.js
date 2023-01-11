ad60d736fa49d58aa9b5445f84a0c709139a600c
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-11 14:58:37 -0500

    added users admin route protection

---
+++ client/features/users/Users.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { fetchUsersAsync, selectUsers } from "./usersSlice";
+
+const Users = () => {
+  const users = useSelector(selectUsers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsersAsync());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {users && users.length ?
+       users.map((user) => (
+        <ul
+        className="allUsers"
+        key={`All Users: ${user.id}`}>
+          <li>
+            {[user.email]}
+          </li>
+        </ul>
+       )): <div>Access denied</div>
+    }
+    </div>
+  )
+};
+
+export default Users;
