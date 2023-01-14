30c85605eae9aee668c15568c04ea0fc290e3f89
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2023-01-14 16:16:26 -0500

    added admin navbar

---
+++ client/features/navbar/AdminNavbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const AdminNavbar = () => {
+	return (
+		<div>
+			<Link to="/users">All users</Link>
+		</div>
+	);
+};
--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,46 +1,58 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout,reset } from '../auth/authSlice';
-import { clearCartOnLogout, selectCart } from '../cart/cartSlice';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout, reset, selectAuth } from "../auth/authSlice";
+import { clearCartOnLogout, selectCart } from "../cart/cartSlice";
+import { AdminNavbar } from "./AdminNavbar";
 const Navbar = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-  const cart = useSelector(selectCart)
-  const dispatch = useDispatch();
+	const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+	const { isAdmin } = useSelector(selectAuth);
+	const cart = useSelector(selectCart);
+	const dispatch = useDispatch();
+	const cartQuantity = cart.length
+		? cart.reduce((total, { quantity }) => total + quantity, 0)
+		: 0;
+	const onLogout = () => {
+		dispatch(logout());
+		dispatch(reset());
+		dispatch(clearCartOnLogout());
+	};
 
-  const cartQuantity = cart.length ? cart.reduce((total, {quantity})=> total+ quantity,0) : 0
-  const onLogout = () => {
-    dispatch(logout());
-    dispatch(reset());
-    dispatch(clearCartOnLogout())
-  };
-
-  return (
-    <div>
-      <h1>FS-App-Template</h1>
-      <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <Link to="/home" onClick={onLogout}>Logout</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart:{cartQuantity}</Link>
-            
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart:{cartQuantity}</Link>
-          </div>
-        )}
-      </nav>
-      <hr />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Grace Shopper Store</h1>
+			<nav>
+				{" "}
+				{isAdmin ? (
+					<div>
+						<AdminNavbar />
+					</div>
+				) : null}
+			</nav>
+			<nav>
+				{isLoggedIn ? (
+					<div>
+						{/* The navbar will show these links after you log in */}
+						<Link to="/home">Home</Link>
+						<Link to="/home" onClick={onLogout}>
+							Logout
+						</Link>
+						<Link to="/products">Products</Link>
+						<Link to="/cart">Cart:{cartQuantity}</Link>
+					</div>
+				) : (
+					<div>
+						{/* The navbar will show these links before you log in */}
+						<Link to="/login">Login</Link>
+						<Link to="/signup">Sign Up</Link>
+						<Link to="/products">Products</Link>
+						<Link to="/cart">Cart:{cartQuantity}</Link>
+					</div>
+				)}
+			</nav>
+			<hr />
+		</div>
+	);
 };
 
 export default Navbar;
