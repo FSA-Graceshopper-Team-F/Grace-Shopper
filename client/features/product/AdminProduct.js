2e083e8abd53d9aa04dc3fcc79774d448ae9e090
Lionel Verrier <lgv3lee@gmail.com>
2023-01-15 22:26:49 -0500

    cleaned up commented code

--- client/features/product/AdminProduct.js
+++ client/features/product/AdminProduct.js
@@ -2,26 +2,15 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { addProductAsync } from "./productSlice";
-//import { fetchProductAsync, selectSingleProduct } from "./singleProductSlice";
 import { Link, useNavigate } from "react-router-dom";
-//import { selectAuth } from "../auth/authSlice";
 
 const AdminProduct = () => {
-
-  //const {id} = useSelector(selectAuth)
-	//const product = useSelector(selectSingleProduct);
   const [name, setName] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
-	//const { productId } = useParams();
 	const dispatch = useDispatch();
- // const navigate = useNavigate();
-	//const { name, price, imageUrl, description } = product;
-	// useEffect(() => {
-	// 	dispatch(fetchProductAsync(productId));
-	// }, [dispatch]);
-
+  
   const onSubmit = (e) => {
     e.preventDefault()
     dispatch(addProductAsync({name, imageUrl, price, description }))
